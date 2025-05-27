00c051c82068f6ce8836e5337c936e4d78ed66a0
Колесник Сергей <serg@astra-live>
2025-05-27 17:03:35 +0300

    create app orders, cart

--- myshop/myshop/settings.py
+++ myshop/myshop/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     "cart.apps.CartConfig",
     "shop.apps.ShopConfig",
+    "orders.apps.OrdersConfig",
 ]
 
 MIDDLEWARE = [
@@ -63,6 +64,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                "cart.context_processors.cart"
             ],
         },
     },
--- myshop/shop/views.py
+++ myshop/shop/views.py
@@ -1,8 +1,8 @@
-from django.shortcuts import (
-    render,
-    get_object_or_404,
-)
+from django.shortcuts import get_object_or_404, render
+
+from cart.forms import CartAddProductForm
 from .models import Category, Product
+
 
 def product_list(request, category_slug=None):
     category = None
@@ -13,26 +13,22 @@ def product_list(request, category_slug=None):
         products = products.filter(category=category)
     return render(
         request,
-        "shop/product/list.html",
+        'shop/product/list.html',
         {
-            "category": category,
-            "categories": categories,
-            "products": products,
-        }
+            'category': category,
+            'categories': categories,
+            'products': products,
+        },
     )
+
 
 def product_detail(request, id, slug):
     product = get_object_or_404(
-        Product,
-        id=id,
-        slug=slug,
-        available=True,
+        Product, id=id, slug=slug, available=True
     )
-
+    cart_product_form = CartAddProductForm()
     return render(
         request,
-        "shop/product/detail.html",
-        {
-            "product": product,
-        }
+        'shop/product/detail.html',
+        {'product': product, 'cart_product_form': cart_product_form},
     )
